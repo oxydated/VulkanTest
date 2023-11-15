8bee6e6d75e81d732d183e5c589ad1ecba597a3b
Leandro Almeida de Araujo <oxydated@hotmail.com>
2023-11-15 17:03:42 -0300

    Presentation

--- HelloTriangleApplication.cpp
+++ HelloTriangleApplication.cpp
@@ -781,6 +781,20 @@ void HelloTriangleApplication::drawFrame() {
 	if (vkQueueSubmit(graphicsQueue, 1, &submitInfo, inFlightFence) != VK_SUCCESS) {
 		throw std::runtime_error("failed to submit draw command buffer!");
 	}
+
+	VkPresentInfoKHR presentInfo{};
+	presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+	presentInfo.waitSemaphoreCount = 1;
+	presentInfo.pWaitSemaphores = signalSemaphores;
+
+	VkSwapchainKHR swapChains[] = { swapChain };
+	presentInfo.swapchainCount = 1;
+	presentInfo.pSwapchains = swapChains;
+	presentInfo.pImageIndices = &imageIndex;
+
+	presentInfo.pResults = nullptr;
+
+	vkQueuePresentKHR(presentQueue, &presentInfo);
 }
 
 void HelloTriangleApplication::mainLoop() {
